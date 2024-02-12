c28141857a59e1fecd4c888b5a93c4453544e141
Nils Kriedner <nkriedner@googlemail.com>
2024-02-12 17:10:23 +0100

    Create js logic for t-shirt info section

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const nameInput = document.querySelector("#name");
 const jobRoleSelect = document.querySelector("#title");
 const otherJobRole = document.querySelector("#other-job-role");
+const designSelect = document.querySelector("#design");
+const colorSelect = document.querySelector("#color");
 
 // When page loads name input has focus:
 nameInput.focus();
@@ -15,7 +17,36 @@ jobRoleSelect.addEventListener("change", (e) => {
 
     // Show hidden 'Other Job' input when 'Other' option is selected:
     if (selectValue === "other") {
-        console.log("show");
         otherJobRole.hidden = false;
+        otherJobRole.focus();
+    }
+});
+
+// Disable color select by default:
+colorSelect.disabled = true;
+
+// Event listener for 'Desgin' select:
+designSelect.addEventListener("change", (e) => {
+    const selectValue = e.target.value;
+    // console.log(selectValue);
+
+    // Enable color select
+    colorSelect.disabled = false;
+
+    // Loop through the option elements and hide all options not matching the selecValue
+    const colorOptions = document.querySelectorAll("#color option");
+    // console.log(colorOptions);
+    for (let i = 0; i < colorOptions.length; i++) {
+        // console.log(colorOptions[i].getAttribute("data-theme"));
+
+        // If the color option is not selected value -> hide it
+        if (colorOptions[i].getAttribute("data-theme") !== selectValue) {
+            // console.log(colorOptions[i].value);
+            colorOptions[i].hidden = true;
+        } else {
+            // show it:
+            colorOptions[i].hidden = false;
+            colorSelect.value = colorOptions[i].value; // preselects in every loop -> not ideal solution but temporarily ok
+        }
     }
 });
