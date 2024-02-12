ee5e2a9cb3847a1500e0539ce86765178b7167a1
Nils Kriedner <nkriedner@googlemail.com>
2024-02-12 16:13:16 +0100

    Hide/Display other job role input according to job select value

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Element Selectors:
 const nameInput = document.querySelector("#name");
+const jobRoleSelect = document.querySelector("#title");
+const otherJobRole = document.querySelector("#other-job-role");
 
-// When page loads name input has focus
+// When page loads name input has focus:
 nameInput.focus();
+
+// Hide 'Other Job' input on default:
+otherJobRole.hidden = true;
+
+// Event listener for 'Job Role' input:
+jobRoleSelect.addEventListener("change", (e) => {
+    const selectValue = e.target.value;
+
+    // Show hidden 'Other Job' input when 'Other' option is selected:
+    if (selectValue === "other") {
+        console.log("show");
+        otherJobRole.hidden = false;
+    }
+});
